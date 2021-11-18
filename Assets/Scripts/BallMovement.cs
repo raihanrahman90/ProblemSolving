daee61baf8c4427598e8e98f073c8715c596df36
Muhammad Raihan Rahman <11181061@student.itk.ac.id>
2021-11-18 19:51:04 +0800

    add movement

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.AddForce(new Vector2(5000, 5000));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
